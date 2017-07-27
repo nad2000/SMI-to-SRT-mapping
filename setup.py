e6b133006b0ee5a1141a799e07a9a2e7f028775f
Rad Cirskis <nad2000@gmail.com>
2017-07-27 23:17:54 +1200

    setup

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 from setuptools import setup
 
 setup(
-        name="SMI to SRT mapping",
-        version="1.0",
-        py_modules=["map_smi2srt"],
-        install_requires=[
-            "click",
-        ],
-        enrty_points="""
-            [console_scripts]
-            map_smi2srt=map_smi2srt:cli
-        """,
-)
+    name="SMI to SRT mapping",
+    version="1.0",
+    py_modules=["map_smi2srt"],
+    install_requires=[
+        "click",
+    ],
+    enrty_points=dict(
+        console_scripts=["map_smi2srt=map_smi2srt:cli"], ), )
